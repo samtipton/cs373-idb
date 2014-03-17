7a3dd545d601d11aabdcf8d309a32d172d8b462e
Sam Tipton <sam.tipton@gmail.com>
2014-03-17 16:17:59 -0500

    updated models, added venue, roster schemes

--- idb_main/models.py
+++ idb_main/models.py
@@ -1,39 +1,55 @@
 from django.db import models 
-  
-class Game(models.Model) : 
-    winning_team = models.CharField(max_length=200) 
-    losing_team  = models.CharField(max_length=200) 
-    venue        = models.CharField(max_length=200) 
-    score        = models.CharField(max_length=200) 
-    game_number  = models.CharField(max_length=200)
-    location     = models.CharField(max_length=200) 
-    attendance   = models.IntegerField(default = 0) 
-    game_day     = models.DateField('game day') 
-  
-    def __str__ (self) :    
-        return "SuperBowl " + self.game_number 
-  
+
 class Team(models.Model) : 
-  
-    game       = models.ForeignKey(Game) 
-  
-    team_name  = models.CharField(max_length=200) 
-    team_city  = models.CharField(max_length=200) 
-    coach_name = models.CharField(max_length=200) 
-    num_wins   = models.IntegerField(default = 0) 
-      
-  
+    team_name  = models.CharField(max_length = 500) 
+    team_city  = models.CharField(max_length = 500)
+    owner = models.CharField(max_length = 500)
+
     def __str__ (self) : 
         return self.team_name 
-  
+
 class Player(models.Model) : 
-  
-    team       = models.ForeignKey(Team) 
-  
-    first_name = models.CharField(max_length=200) 
-    last_name  = models.CharField(max_length=200) 
-    position   = models.CharField(max_length=200) 
-    number     = models.IntegerField(default = 0) 
+    first_name = models.CharField(max_length = 500) 
+    last_name = models.CharField(max_length = 500) 
+    birth_date = models.CharField(max_length = 500) 
+    birth_town = models.CharField(max_length = 500) 
+    high_school = models.CharField(max_length = 500) 
+    college = models.CharField(max_length = 500) 
   
     def __str__ (self) : 
         return self.first_name + " " + self.last_name
+
+class Venue(models.Model) : 
+    name = models.CharField(max_length = 500)
+    address = models.CharField(max_length = 500) 
+    city = models.CharField(max_length = 500) 
+    state = models.CharField(max_length = 500) 
+    zip = models.CharField(max_length = 500) 
+
+    def __str__(self) : 
+        return self.name    
+
+class Game(models.Model) : 
+    home_team = models.ForeignKey(Team, related_name = "home_team")
+    away_team = models.ForeignKey(Team, related_name = "away_team")
+    home_score = models.IntegerField(default = 0)
+    away_score = models.IntegerField(default = 0)
+    venue = models.ForeignKey(Venue)
+    game_day = models.DateField()
+    game_number = models.CharField(max_length = 500)
+  
+    def __str__ (self) :    
+        return "SuperBowl " + self.game_number 
+
+class Roster(models.Model) : 
+    player = models.ForeignKey(Player)
+    team = models.ForeignKey(Team)
+    position = models.CharField(max_length = 500)
+    player_number = models.CharField(max_length = 500)
+    year = models.IntegerField()
+
+    def __str__(self) : 
+        return self.team.team_name
+
+
+
