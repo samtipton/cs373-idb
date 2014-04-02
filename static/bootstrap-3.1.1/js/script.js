5f51ef049fd2653422ee66b3d02601872470e4af
Sam Tipton <sam.tipton@gmail.com>
2014-04-02 12:27:00 -0500

    added dynamic styling for navbar

--- static/bootstrap-3.1.1/js/script.js
+++ static/bootstrap-3.1.1/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+
+	var bindex;
+	var page = getCurentFileName()
+
+	switch(page) {
+		case "games/" :   bindex = 2; break;
+		case "teams/" :   bindex = 3; break;
+		case "players/" : bindex = 4; break;
+		case "contact/" : bindex = 5; break;
+		default : break;
+	}
+
+	// activate correct nav button
+	if (bindex != null) {
+		var active_element = '.masthead ul li:nth-child(' + bindex.toString() + ')';
+		$(active_element).addClass('active');
+	}
+
 	// navbar animation
 	$('.masthead ul li').mouseenter(function() {
 		$( this ).addClass( 'active' );
@@ -7,4 +25,9 @@ $(document).ready(function() {
 	$('.masthead ul li').mouseleave(function() {
 		$( this ).removeClass( 'active' );
 	});
+
+	function getCurentFileName() {
+	    var pagePathName= window.location.pathname;
+	    return pagePathName.substring(pagePathName.indexOf("/") + 1);
+	}
 });
