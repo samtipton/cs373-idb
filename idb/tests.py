1cae84e9143f1022cf92b5c6c54f25bec2c700b8
Yoan Chinique <ychinique@gmail.com>
2014-04-03 14:51:35 -0500

    Added more helper functions.

--- idb/tests.py
+++ idb/tests.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen, Request
 from json import dumps, loads
 from idb.models import MVP, Franchise, SuperBowl
 import types
+from django.db.models import Q
 
 # ----------------------
 # RESTful API Unit Tests
@@ -10,14 +11,22 @@ import types
 # ----------------------
 
 def merge(*dicts):
+	"""
+	This is a helper function to merge multiple dictionaries and return
+	a new merged dictionary.
+	"""
 	result = {}
-	for d in *dicts:
+	for d in dicts:
 		result.update(d)
 	return result
 
 class test_API(TestCase) :
 
 	def assert_successful_response(self, api_response):
+		"""
+		This is a helper function to assert a successful response from the
+		API. Additionally, it returns the payload of the API operation.
+		"""
 		self.assertTrue(type(api_response) is dict)
 		self.assertTrue("success" in api_response)
 		self.assertTrue(api_response["success"])
@@ -25,6 +34,10 @@ class test_API(TestCase) :
 		return api_response["data"]
 
 	def assert_failure_response(self, api_response, error_type, error_message):
+		"""
+		This is a helper function to asser that an API operation failed and
+		that it failed with the expected error response.
+		"""
 		self.assertTrue(type(api_response) is dict)
 		self.assertTrue("success" in api_response)
 		self.assertFalse(api_response["success"])
@@ -36,19 +49,75 @@ class test_API(TestCase) :
 		self.assertEqual(api_response["error"]["message"], error_message)
 
 	def path(self, _type = "", id = None):
+		"""
+		This is a helper function to compute the url to an API operation.
+		"""
 		p = "/api/v2/" + str(_type)
 		if id is not None:
-		p += "/" + str(id)
+			p += "/" + str(id)
 		return p
 
 	def ref(self, model):
-		_type = str(model.__name__).lower() + "s"
+		"""
+		This is a helper function to generate the API urls for a specific
+		model instance.
+		"""
+		_type = str(type(model).__name__).lower() + "s"
 		return {
 			"id": model.id,
 			"self": self.path(_type, model.id),
 			"collection": self.path(_type)
 		}
-
+	
+	def serialize_superbowl_model(self, model):
+		return merge(self.ref(model), {
+		    "winning_franchise": self.ref(model.winning_franchise),
+		    "losing_franchise": self.ref(model.winning_franchise),
+		    "mvp": self.ref(model.winning_franchise),
+		    "mvp_stats": model.mvp_stats,
+		    "mvp_blurb": model.mvp_blurb,
+		    "winning_score": model.winning_score,
+		    "losing_score": model.losing_score,
+		    "venue_name": model.venue_name,
+		    "venue_city": model.venue_city,
+		    "venue_state": model.venue_state,
+		    "game_day": str(model.game_day),
+		    "attendance": model.attendance,
+		    "game_number": model.game_number,
+		    "halftime_performer": model.halftime_performer,
+		    "twitter_id": model.twitter_id,
+		    "youtube_id": model.youtube_id,
+		    "latitude": model.latitude,
+		    "longitude": model.longitude
+		})
+	
+	def serialize_franchise_model(self, model):
+		return merge(self.ref(model), {
+		    "mvps": [self.ref(m) for m in model.mvps.all()],
+			"superbowls": [self.ref(m) for m in ...],
+		    "team_name": ,
+			"team_city": "Seattle",
+			"team_state": "WA",
+			"current_owner": "Paul Allen",
+			"current_gm": "John Schneider",
+			"current_head_coach": "Pete Carroll",
+			"year_founded": 1974,
+			"active": true,
+			"home_stadium": "CenturyLink Field",
+			"division": "NFC West",
+			"facebook_id": "seahawks",
+			"twitter_id": "GoHawks",
+			"youtube_id": "vh500h",
+			"latitude": 334.6,
+			"longitude": 44.1
+		})
+	
+	
+	
+	# ----------------------------------------------------
+	# Helper functions to make REST calls against the API.
+	# ----------------------------------------------------
+	
 	def get(self, _type = "", id = None):
 		url = path(_type, id)
 		response = self.client.get(url)
@@ -72,21 +141,44 @@ class test_API(TestCase) :
 		response = self.client.get(url)
 		api_response = loads(response.content)
 		return (response.status_code, api_response)
-
 	
-	# -----------------------------------------
-	# get
-	# -----------------------------------------
-	# check status codes and content is correct
-	# ----------------------------------------- 
+	# ----------
+	# Unit Tests
+	# ----------
 	
 	def setUp(self) :
+		# Initialize the database for testing.
 		self.malcolm_smith = MVP.objects.create(first_name='Malcolm', last_name='Smith', position='OLB', birth_date='1989-07-05', birth_town='Woodland Hills, CA', high_school='Woodland Hills (CA) Taft', college='Southern California', draft_year=2011, active=True, salary=465000, facebook_id='MalcSmitty', twitter_id='MalcSmitty', youtube_id='zfB8hCsHwLE', latitude=34.1683, longitude=-118.6050)
 		self.seahawks = Franchise.objects.create(team_name='Seahawks', team_city='Seattle', team_state='WA', current_owner='Paul Allen', current_gm='John Schneider', current_head_coach='Pete Carroll', year_founded=1974, active=True, home_stadium='CenturyLink Field', division='NFC West', facebook_id='Seahawks', twitter_id='Seahawks', youtube_id='l9-NicPH-58', latitude=47.5953, longitude=-122.3317)
 		self.broncos = Franchise.objects.create(team_name='Broncos', team_city='Denver', team_state='CO', current_owner='Pat Bowlen', current_gm='John Elway', current_head_coach='John Fox', year_founded=1960, active=True, home_stadium='Sports Authority Field', division='AFC West', facebook_id='DenverBroncos', twitter_id='Broncos', youtube_id='HGMbmbnu2Oc', latitude=39.7439, longitude=-105.0200)
 		self.seahawks.mvps.add(self.malcolm_smith)
 		self.sb_48 = SuperBowl.objects.create(winning_franchise=self.seahawks, losing_franchise=self.broncos, mvp=self.malcolm_smith, mvp_stats='1 INT 1 FR 1 TD 9 T', mvp_blurb='Malcolm Smith was the de-facto SB MVP for a legendary Seattle defense.\n\nWhile he was a relative unknown at the start of the season, a game-sealing interception in the NFC Championship as well as two takeaways in the Super Bowl served as a coming out party for the young linebacker.', winning_score=43, losing_score=8, venue_name='MetLife Stadium', venue_city='East Rutherford', venue_state='NJ', game_day='2014-02-02', attendance=82529, game_number='XLVIII', halftime_performer='Bruno Mars', twitter_id='SuperBowlXLVIII', youtube_id='NbcA1UISfG0', latitude=40.8136, longitude=-74.0744)
 		self.client = Client()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	def test_get_superbowls(self) :
 		response = self.client.get('/api/v2/superbowls')
