9a2bb09d45ae31fcc6999592f1c3380e81f02db7
Sam Tipton <sam.tipton@gmail.com>
2014-04-10 15:49:11 -0500

    views can now handle invalid url requests gracefully with a 404 message page

--- idb/views.py
+++ idb/views.py
@@ -23,11 +23,16 @@ def splash(request) :
 def superbowls(request, id = None) :
 
 	if id.isdigit():
-		url = 'idb/superbowl-template.html'
-		game_id = int(id)
-		game = SuperBowl.objects.get(id = game_id)
-		mvp = MVP.objects.get(id = game.mvp.id)
-		context = RequestContext(request, {'game':game, 'mvp':mvp})
+		if int(id) < len(SuperBowl.objects.all()) :
+			url = 'idb/superbowl-template.html'
+			game_id = int(id)
+			game = SuperBowl.objects.get(id = game_id)
+			mvp = MVP.objects.get(id = game.mvp.id)
+			context = RequestContext(request, {'game':game, 'mvp':mvp})
+		else :
+			url = 'idb/404.html'
+			context = RequestContext(request)
+
 	else :
 		url = 'idb/superbowls-template.html'
 		game_list = SuperBowl.objects.order_by('-game_day')
@@ -40,13 +45,17 @@ def superbowls(request, id = None) :
 def franchises(request, id = None) :
 
 	if id.isdigit():
-		url = 'idb/franchise-template.html'
-		team_id = int(id)
-		team = Franchise.objects.get(id = team_id)
-		game_history = SuperBowl.objects.filter(Q(winning_franchise=team) | Q(losing_franchise=team)).all()
-		mvp_history = team.mvps.all()
-		context = RequestContext(request, {'team':team, 'game_history':game_history
-			,'mvp_history':mvp_history})
+		if int(id) < len(Franchise.objects.all()) :
+			url = 'idb/franchise-template.html'
+			team_id = int(id)
+			team = Franchise.objects.get(id = team_id)
+			game_history = SuperBowl.objects.filter(Q(winning_franchise=team) | Q(losing_franchise=team)).all()
+			mvp_history = team.mvps.all()
+			context = RequestContext(request, {'team':team, 'game_history':game_history
+				,'mvp_history':mvp_history})
+		else :
+			url = 'idb/404.html'
+			context = RequestContext(request)
 	else :
 		url = 'idb/franchises-template.html'
 		team_list = Franchise.objects.order_by('-year_founded')
@@ -60,12 +69,17 @@ def franchises(request, id = None) :
 def mvps(request, id = None) :
 
 	if id.isdigit():
-		url = 'idb/mvp-template.html'
-		mvp_id = int(id)
-		mvp = MVP.objects.get(id=mvp_id)
-		game_history = SuperBowl.objects.filter(mvp=mvp).all()
-		teams = Franchise.objects.filter(mvps__in=[mvp]).all()
-		context = RequestContext(request, {'mvp':mvp, 'game_history':game_history, 'teams':teams})
+		if int(id) < len(MVP.objects.all()) :
+			url = 'idb/mvp-template.html'
+			mvp_id = int(id)
+			mvp = MVP.objects.get(id=mvp_id)
+			game_history = SuperBowl.objects.filter(mvp=mvp).all()
+			teams = Franchise.objects.filter(mvps__in=[mvp]).all()
+			context = RequestContext(request, {'mvp':mvp, 'game_history':game_history, 'teams':teams})
+		else :
+			url = 'idb/404.html'
+			context = RequestContext(request)
+
 	else :
 		url = 'idb/mvps-template.html'
 		mvp_list = MVP.objects.order_by('-draft_year')
